9623f43e5933aeba28cca92a0c3e01dd03f81737
Jibril Nikki <jibrilnikki.g@gmail.com>
2025-08-05 08:48:41 +0700

    Update kota_banjar.py

--- Project_BPS/views/kota_banjar.py
+++ Project_BPS/views/kota_banjar.py
@@ -405,11 +405,6 @@ update_rekap_total_landmark(df_merged, "Kota Banjar", conn_st.engine)
 
 st.subheader("Filter Data")
 
-st.markdown(
-    f"<span style='font-size: 20px; color: black;'>{match:,} baris cocok. {unmatched:,} baris belum ada di database.</span>",
-    unsafe_allow_html=True
-)
-
 col11, col22, col33, col44 = st.columns(4)
 
 filtered_df = df_merged
